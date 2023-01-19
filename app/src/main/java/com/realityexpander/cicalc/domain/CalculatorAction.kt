ebdb403b8e52031a8a7be165ec0d551da70d824f
Chris Athanas <realityexpander@gmail.com>
2023-01-19 00:15:21 -0600

    Add ExpressionWriter.kt & ExpressionWriterTest.kt

---
+++ app/src/main/java/com/realityexpander/cicalc/domain/CalculatorAction.kt
@@ -0,0 +1,11 @@
+package com.realityexpander.cicalc.domain
+
+sealed interface CalculatorAction {
+    data class Number(val number: Int): CalculatorAction
+    data class Op(val operation: Operation): CalculatorAction
+    object Clear: CalculatorAction
+    object Delete: CalculatorAction
+    object Parentheses: CalculatorAction
+    object Calculate: CalculatorAction
+    object Decimal: CalculatorAction
+}
---
+++ app/src/main/java/com/realityexpander/cicalc/domain/ExpressionWriter.kt
@@ -0,0 +1,84 @@
+package com.realityexpander.cicalc.domain
+
+class ExpressionWriter {
+
+    var workExpression = ""
+
+    fun processAction(action: CalculatorAction) {
+        when(action) {
+            CalculatorAction.Calculate -> {
+                val parser = ExpressionParser(prepareForCalculation())
+                val evaluator = ExpressionEvaluator(parser.parse())
+                workExpression = evaluator.evaluate().toString()
+            }
+            CalculatorAction.Clear -> {
+                workExpression = ""
+            }
+            CalculatorAction.Decimal -> {
+                if(canEnterDecimal()) {
+                    workExpression += "."
+                }
+            }
+            CalculatorAction.Delete -> {
+                workExpression = workExpression.dropLast(1)
+            }
+            is CalculatorAction.Number -> {
+                workExpression += action.number
+            }
+            is CalculatorAction.Op -> {
+                if(canEnterOperation(action.operation)) {
+                    workExpression += action.operation.symbol
+                }
+            }
+            CalculatorAction.Parentheses -> {
+                processParentheses()
+            }
+        }
+    }
+
+    private fun prepareForCalculation(): String {
+        val newExpression = workExpression.takeLastWhile {
+            it in "$operationSymbols(."
+        }
+        if(newExpression.isEmpty()) {
+            return "0"
+        }
+
+        return newExpression
+    }
+
+    private fun processParentheses() {
+        val openingCount = workExpression.count { it == '(' }
+        val closingCount = workExpression.count { it == ')' }
+
+        workExpression += when {
+            workExpression.isEmpty() ||
+                workExpression.last() in "$operationSymbols(" -> "("
+
+            workExpression.last() in "0123456789)" &&
+                    openingCount == closingCount -> return
+
+            else -> ")"
+        }
+    }
+
+    private fun canEnterDecimal(): Boolean {
+        if(workExpression.isEmpty() || workExpression.last() in "$operationSymbols.()") {
+            return false
+        }
+
+        return !workExpression.takeLastWhile {
+            it in "0123456789."
+        }.contains(".")
+    }
+
+    private fun canEnterOperation(operation: Operation): Boolean {
+        if(operation in listOf(Operation.PLUS, Operation.MINUS)
+            && workExpression.isEmpty()
+        ) {
+            return workExpression.isEmpty() || workExpression.last() in "$operationSymbols()0123456789"
+        }
+
+        return workExpression.isNotEmpty() || workExpression.last() in "0123456789)"
+    }
+}
