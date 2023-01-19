1585a800d807e7de6e7292b91aa829a776721234
Chris Athanas <realityexpander@gmail.com>
2023-01-19 00:56:22 -0600

    Add functionality for screen

---
+++ app/src/main/java/com/realityexpander/cicalc/presentation/CalculatorUiAction.kt
@@ -0,0 +1,18 @@
+package com.realityexpander.cicalc.presentation
+
+import androidx.compose.runtime.Composable
+import com.realityexpander.cicalc.domain.CalculatorAction
+
+data class CalculatorUiAction(
+    val text: String?,
+    val highlightLevel: HighlightLevel,
+    val action: CalculatorAction,
+    val content: @Composable () -> Unit = {}
+)
+
+sealed interface HighlightLevel {
+    object Neutral: HighlightLevel
+    object SemiHighlighted: HighlightLevel
+    object Highlighted: HighlightLevel
+    object StronglyHighlighted: HighlightLevel
+}
