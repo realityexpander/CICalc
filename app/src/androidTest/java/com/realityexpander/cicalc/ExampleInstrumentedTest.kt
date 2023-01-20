b50b7272efff6e32f83209ee75f1a23cd596c6dc
Chris Athanas <realityexpander@gmail.com>
2023-01-19 21:57:35 -0600

    Add application ID printout

--- app/src/androidTest/java/com/realityexpander/cicalc/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/realityexpander/cicalc/ExampleInstrumentedTest.kt
@@ -19,6 +19,9 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+
+        println("BuildConfig.APPLICATION_ID: ${BuildConfig.APPLICATION_ID}")
+
         assertEquals(BuildConfig.APPLICATION_ID, appContext.packageName)
     }
 }
