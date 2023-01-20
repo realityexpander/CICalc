7767f50810712bb14f67f580bc3ce6c40b144dbc
Chris Athanas <realityexpander@gmail.com>
2023-01-19 21:48:52 -0600

    Fix build.gradle versions

--- app/src/androidTest/java/com/realityexpander/cicalc/presentation/CalculatorScreenTest.kt
+++ app/src/androidTest/java/com/realityexpander/cicalc/presentation/CalculatorScreenTest.kt
@@ -2,7 +2,6 @@ package com.realityexpander.cicalc.presentation
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import com.realityexpander.cicalc.MainActivity
